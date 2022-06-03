a8e2db66cdfeb81dbc7436e33eeb4106ed5b7977
FynnCr <105922200+FynnCr@users.noreply.github.com>
2022-06-03 08:19:17 +0200

    Create resources.js

---
+++ resources.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright 2020 Amazon.com, Inc. and its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: LicenseRef-.amazon.com.-AmznSL-1.0
+ *
+ * Licensed under the Amazon Software License (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ * http://aws.amazon.com/asl/
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ **/
+module.exports = {
+    en: {
+        translation: {
+            ERROR_RESPONSE: `Sorry, I had trouble doing what you asked. Please try again.`
+        }
+    },
+    de: {
+        translation: {
+            ERROR_RESPONSE: `Entschuldigung, ich hatte Probleme das zu tun, wonach du gefragt hast. Bitte versuche es erneut.`
+        }
+    }
+};
